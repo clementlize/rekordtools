5c881ac680caf1582e59c21485dad5bc42e40469
Clément <clement.lize1@gmail.com>
2022-01-21 14:52:23 +0100

    WIP changed webpack rules, new bugs

--- webpack/rules.webpack.js
+++ webpack/rules.webpack.js
@@ -1,5 +1,31 @@
 module.exports = [
     {
+        // We're specifying native_modules in the test because the asset relocator loader generates a
+        // "fake" .node file which is really a cjs file.
+        test: /native_modules\/.+\.node$/,
+        use: 'node-loader',
+    },
+    {
+        test: /\.(m?js|node)$/,
+        parser: { amd: false },
+        use: {
+            loader: '@vercel/webpack-asset-relocator-loader',
+            options: {
+                outputAssetBase: 'native_modules',
+            },
+        }
+    },
+    /*{
+        test: /\.tsx?$/,
+        exclude: /(node_modules|\.webpack)/,
+        use: {
+            loader: 'ts-loader',
+            options: {
+                transpileOnly: true,
+            },
+        },
+    },*/
+    /*{
         test: /\.node$/,
         use: 'node-loader',
     },
@@ -15,7 +41,7 @@ module.exports = [
         resolve: {
             fullySpecified: false,
         }
-    },
+    },*/
     {
         test: /\.(js|ts|tsx)$/,
         exclude: /node_modules/,
