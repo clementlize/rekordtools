ba5a6a0f3eec282ff29e2258aec68ba50ff7a3f6
Clément <clement.lize1@gmail.com>
2022-01-24 20:34:39 +0100

    build: added electron-squirrel-startup for windows installer

--- electron/main.ts
+++ electron/main.ts
@@ -8,6 +8,12 @@ let mainWindow: BrowserWindow | null
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string
 
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+    // eslint-disable-line global-require
+    app.quit();
+}
+
 // const assetsPath =
 //   process.env.NODE_ENV === 'production'
 //     ? process.resourcesPath
