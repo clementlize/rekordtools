d4c258ff8be6cdf2581be94d8726a2140affb7e8
Clément <clement.lize1@gmail.com>
2022-01-24 19:32:48 +0100

    build: added minimal MUI example (with Roboto font)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import { GlobalStyle } from './styles/GlobalStyle'
+import { Box, CssBaseline, ThemeProvider, Typography } from '@mui/material';
+import 'typeface-roboto';
+import { Greetings } from './components/Greetings';
+import { defaultTheme } from './config/theme';
+import { GlobalStyle } from './styles/GlobalStyle';
 
-import { Greetings } from './components/Greetings'
 
 export function App() {
 
-  window.Main.on("response", (data: string) => {
+    window.Main.on("response", (data: string) => {
 
-    console.log(data);
-})
+        console.log(data);
+    })
 
-  return (
-    <>
-      <GlobalStyle />
-      <Greetings />
-    </>
-  )
+    return (
+        <ThemeProvider theme={defaultTheme}>
+
+            <CssBaseline />
+
+            <GlobalStyle />
+            <Greetings />
+
+            <Box height={1}>
+
+                <Typography variant="h3">Welcome to rekordtools.</Typography>
+
+            </Box>
+
+        </ThemeProvider>
+    )
 }
