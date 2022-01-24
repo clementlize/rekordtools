3af6b347321127abc42048eb5d68567f95467d30
Clément <clement.lize1@gmail.com>
2022-01-24 14:11:06 +0100

    build: added sqlite3

--- webpack/main.webpack.js
+++ webpack/main.webpack.js
@@ -1,9 +1,22 @@
 module.exports = {
   resolve: {
-    extensions: ['.ts', '.js']
+      extensions: ['.ts', '.js']
   },
   entry: './electron/main.ts',
   module: {
-    rules: require('./rules.webpack'),
+      rules: [
+          ...require('./rules.webpack'),
+
+          {
+              test: /\.(m?js|node)$/,
+              parser: { amd: true },
+              use: {
+                  loader: '@vercel/webpack-asset-relocator-loader',
+                  options: {
+                      outputAssetBase: 'native_modules',
+                  },
+              }
+          },
+      ],
   }
 }
--- webpack/rules.webpack.js
+++ webpack/rules.webpack.js
@@ -1,30 +1,31 @@
 module.exports = [
   {
-    test: /\.node$/,
-    use: 'node-loader',
+      // We're specifying native_modules in the test because the asset relocator loader generates a
+      // "fake" .node file which is really a cjs file.
+      test: /native_modules\/.+\.node$/,
+      use: 'node-loader',
   },
   {
-    test: /\.(m?js|node)$/,
-    parser: { amd: false },
-    use: {
-      loader: '@marshallofsound/webpack-asset-relocator-loader',
+      test: /\.(js|ts|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+          loader: 'babel-loader',
+      }
+  },
+  {
+      test: /\.(png|jpe?g|gif)$/i,
+      loader: 'file-loader',
       options: {
-        outputAssetBase: 'native_modules',
+          name: '[path][name].[ext]',
       },
-    },
   },
   {
-    test: /\.(js|ts|tsx)$/,
-    exclude: /node_modules/,
-    use: {
-      loader: 'babel-loader'
-    }
+      test: /\.(sass|less|css)$/,
+      use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+      ]
   },
-  {
-    test: /\.(png|jpe?g|gif)$/i,
-    loader: 'file-loader',
-    options: {
-      name: '[path][name].[ext]',
-    },
-  }
 ]
