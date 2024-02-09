71a5d00844eed45a4c8cedd6d606af2ea74aa633
clementlize <clement.lize1@gmail.com>
2024-02-09 11:11:55 +0100

    final: using encryption to get db key

--- electron/database/database.ts
+++ electron/database/database.ts
@@ -1,8 +1,8 @@
 import { Database } from "@journeyapps/sqlcipher";
+import { runBytecodeFile } from "bytenode";
 import Blowfish from "egoroof-blowfish";
 import { IpcMainEvent } from "electron/main";
 import fs from 'fs';
-import path from 'path';
 import { getRekordboxSettingsPath } from "./rekordBoxSettingsPath";
 
 const sqlite3 = require("@journeyapps/sqlcipher").verbose();
@@ -15,88 +15,74 @@ const getRekordBoxSettings = (): (string[])[] => {
     return JSON.parse(fs.readFileSync(getRekordboxSettingsPath(), "utf-8")).options;
 }
 
+
 /**
  * Extract and return the database key
  * @returns 
  */
-const getDbKey = (): string | undefined => {
+const getDbKey = (): string => {
 
-    const getEncryptedPassword = (_asarPath: string): string | undefined => {
+    const bytecodeFilePath0 = process.cwd() + "/bd8f13e1-e7bb-46b6-9cbc-aa9955e596e1.jsc";
+    const tempKey = runBytecodeFile(bytecodeFilePath0);
+    return tempKey;
 
-        const authManager = fs.readFileSync(path.resolve(_asarPath, "controllers/auth_manager.js"), "utf-8");
-
-        let password: string | undefined = undefined;
-        authManager.split(/\r?\n/).forEach(line => {
-
-            if (line.includes("pass: ")) {
-
-                password = line.split(": ")[1].split("\"")[1];
-            }
-        });
-
-        return password;
-    };
-
-    const decrypt = (_passwordEncoded: Buffer, _key: string): string => {
-
-        const bf = new Blowfish(_key, Blowfish.MODE.ECB);
-        return bf.decode(_passwordEncoded, Blowfish.TYPE.STRING);
-    };
-
-    const encodedKey = getRekordBoxSettings().find(x => x[0] === "dp")?.[1];
-    if (!encodedKey) {
-        throw new Error("Could not find encoded key in config file");
-    }
-    console.log(`Encoded key: ${encodedKey}`);  // TODO: remove
-
+    const bytecodeFilePath1 = process.cwd() + "/943cd794-6ffe-48b8-9db4-303a80c46932.jsc";
+    const encodedKey = runBytecodeFile(bytecodeFilePath1);
     const key = Buffer.from(encodedKey, "base64");
 
-    // TODO: what happens on OSX?
-    const passwordString = getEncryptedPassword(asarPath);
-
-    if (!passwordString) {
-
-        console.log("Error: could not find asar password");
-        return "err";
-    }
+    const bytecodeFilePath2 = process.cwd() + "/d49c21fd-00e1-459e-a24a-22fd068e3815.jsc";
+    const password = runBytecodeFile(bytecodeFilePath2);
 
     // The final decrypted key
-    return decrypt(key, passwordString);
+    const bf = new Blowfish(key, Blowfish.MODE.ECB);
+    const dbKey = bf.decode(password, Blowfish.TYPE.STRING);
+    console.log(`Decrypted key`, {  // TODO: remove
+        encodedKey,
+        password,
+        dbKey,
+    });
+    return dbKey;
 }
 
 /**
  * Get the database path based on the settings file
  */
 const getDbPath = (): string => {
-
     return getRekordBoxSettings()[0][1];
 }
 
-export const readDb = (event: IpcMainEvent) => {
+export const readDb = async (event: IpcMainEvent) => {
 
-    //const db: Database = new sqlite3.Database("E:\\dev\\perso\\files\\master.db", () => {
-    //const db: Database = new sqlite3.Database("/mnt/datadev/perso/files/master.db", () => {
-    //const db: Database = new sqlite3.Database("/mnt/data/dev/perso/files/master.db", () => {
-    const db: Database = new sqlite3.Database(getDbPath(), () => {
+    try {
 
-        db.serialize(() => {
+        const dbPath = getDbPath();
+        const dbKey = getDbKey();  // TODO: try catch here, return error to webapp
 
-            db.run("PRAGMA cipher_compatibility = 4");
-            db.run(`PRAGMA key = '${getDbKey()}'`);
+        const db: Database = new sqlite3.Database(dbPath, () => {
 
-            db.all("select name from sqlite_master where type='table'", function (err: any, tables: any) {
+            db.serialize(() => {
 
-                console.log("tables:")
-                console.log(tables);
-                event.reply("readDb-response", tables);
-            });
+                db.run("PRAGMA cipher_compatibility = 4");
+                db.run(`PRAGMA key = '${dbKey}'`);
 
-            db.each("SELECT ID AS id, Name, Seq FROM djmdPlaylist", function (err: any, row: any) {
+                db.all("select name from sqlite_master where type='table'", function (err: any, tables: any) {
 
-                console.log(err);
-                console.log(row);
-                //console.log(`ID: ${row.id} ; Name: ${row.Name} ; Seq : ${row.Seq}`);
+                    console.log("tables:")
+                    console.log(tables);
+                    event.reply("readDb-response", tables);
+                });
+
+                db.each("SELECT ID AS id, Name, Seq FROM djmdPlaylist", function (err: any, row: any) {
+
+                    console.log(err);
+                    console.log(row);
+                    //console.log(`ID: ${row.id} ; Name: ${row.Name} ; Seq : ${row.Seq}`);
+                });
             });
         });
-    });
+    }
+    catch (err) {
+        console.log(err);
+        event.reply("readDb-response", "error");
+    }
 }
